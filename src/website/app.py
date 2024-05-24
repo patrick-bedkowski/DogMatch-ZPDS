9606b8d9cbdf064cb4140484b19e21cf59cc5482
bedkowski.patrick <bedkowskisite@gmail.com>
2024-05-24 19:46:49 +0200

    Working streamlit locally

--- src/website/app.py
+++ src/website/app.py
@@ -12,8 +12,6 @@ import pandas as pd
 import streamlit as st
 import json
 
-from PIL import Image
-
 # Load recommender
 pickle_in = open("../recommender/model.pkl", "rb")
 recommender = pickle.load(pickle_in)
@@ -28,39 +26,38 @@ def welcome():
 def predict_breed(user_input: np.array):
     with open("../recommender/code_to_breed.json", "r") as json_file:
         code_to_breed = json.load(json_file)
-    prediction = recommender.predict(user_input)[0]
-    breed = code_to_breed[prediction]
+    prediction = recommender.predict([user_input])[0]
+    breed = code_to_breed[str(prediction)]
     print(breed)
     return breed
 
 
 def main():
-    st.title("Bank Authenticator")
+    st.title("DogMatch")
     html_temp = """
     <div style="background-color:tomato;padding:10px">
-    <h2 style="color:white;text-align:center;">Streamlit Bank Authenticator ML App </h2>
+    <h2 style="color:white;text-align:center;">Streamlit DogMatch ML App </h2>
     </div>
     """
 
     st.markdown(html_temp, unsafe_allow_html=True)
-    import streamlit as st
 
-    affectionate_with_family = st.text_input("Affectionate With Family", "Type Here")
-    good_with_young_children = st.text_input("Good With Young Children", "Type Here")
-    good_with_other_dogs = st.text_input("Good With Other Dogs", "Type Here")
-    shedding_level = st.text_input("Shedding Level", "Type Here")
-    coat_grooming_frequency = st.text_input("Coat Grooming Frequency", "Type Here")
-    drooling_level = st.text_input("Drooling Level", "Type Here")
-    coat_type = st.text_input("Coat Type", "Type Here")
-    coat_length = st.text_input("Coat Length", "Type Here")
-    openness_to_strangers = st.text_input("Openness To Strangers", "Type Here")
-    playfulness_level = st.text_input("Playfulness Level", "Type Here")
-    watchdog_protective_nature = st.text_input("Watchdog/Protective Nature", "Type Here")
-    adaptability_level = st.text_input("Adaptability Level", "Type Here")
-    trainability_level = st.text_input("Trainability Level", "Type Here")
-    energy_level = st.text_input("Energy Level", "Type Here")
-    barking_level = st.text_input("Barking Level", "Type Here")
-    mental_stimulation_needs = st.text_input("Mental Stimulation Needs", "Type Here")
+    affectionate_with_family = st.number_input("Affectionate With Family", 3)
+    good_with_young_children = st.number_input("Good With Young Children", 3)
+    good_with_other_dogs = st.number_input("Good With Other Dogs", 3)
+    shedding_level = st.number_input("Shedding Level", 2)
+    coat_grooming_frequency = st.number_input("Coat Grooming Frequency", 3)
+    drooling_level = st.number_input("Drooling Level", 2)
+    coat_type = st.number_input("Coat Type", 3)
+    coat_length = st.number_input("Coat Length", 2)
+    openness_to_strangers = st.number_input("Openness To Strangers", 3)
+    playfulness_level = st.number_input("Playfulness Level", 2)
+    watchdog_protective_nature = st.number_input("Watchdog/Protective Nature", 3)
+    adaptability_level = st.number_input("Adaptability Level", 2)
+    trainability_level = st.number_input("Trainability Level", 2)
+    energy_level = st.number_input("Energy Level", 3)
+    barking_level = st.number_input("Barking Level", 2)
+    mental_stimulation_needs = st.number_input("Mental Stimulation Needs", 2)
 
     result = ""
 
@@ -71,12 +68,10 @@ def main():
 
     if st.button("Predict"):
         result = predict_breed(user_input)
-    st.success('Most suitable breed is is {}'.format(result))
+    st.success('Most suitable breed is {}'.format(result))
     if st.button("About"):
-        st.text("Lets LEarn")
+        st.text("Let's Learn")
         st.text("Built with Streamlit")
 
 if __name__ == '__main__':
     main()
-
-
