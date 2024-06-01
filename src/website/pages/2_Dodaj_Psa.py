788e50ec6eda309158f042319533faf6dcf1c05b
Szymon Skarzyński <szym.skarzynski@gmail.com>
2024-06-01 03:26:01 +0200

    limit dog description characters

--- src/website/pages/2_Dodaj_Psa.py
+++ src/website/pages/2_Dodaj_Psa.py
@@ -54,7 +54,7 @@ def main():
     )
 
     st.session_state.description = st.text_area(
-        label="Opis"
+        label="Opis", max_chars=500
     )
 
     st.session_state.photo = st.file_uploader(
