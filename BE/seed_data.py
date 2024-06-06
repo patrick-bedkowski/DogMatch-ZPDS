c1463bb2f52e7c07e9eeed5c6f8a27d8db0d92e3
Pawel Baran <pawel.baran2.stud@pw.edu.pl>
2024-06-06 13:52:03 +0200

    dodanie psów do bazki

--- BE/seed_data.py
+++ BE/seed_data.py
@@ -133,20 +133,154 @@ def seedAnimal(session) -> None:
     dogs.append(Animal(
         name="Szarik",
         breed="German Shepherd Dogs",
-        location="T-34-85",
-        description="Pies brygady pancernej",
+        location="T-34-85 Rudy, między Studziankami a Berlinem",
+        description="Choć jest owczarkiem niemieckim, niemcy się go boją. Nie wymaga dużej uwagi, zje cokolwiek mu podasz.\
+              Jak pokażesz się z nim w parku, wszystkie radzieckie łączniczki będą na Ciebie leciały, a gruzińscy mechanicy będą mieli do Ciebie respekt. \
+              Dobry do polowania na tygrysy syberyjskie",
         photo=read_photo("data/photos/szarik.jpg"),
         owner_id=0
     ))
 
     dogs.append(Animal(
-        name="TEST",
-        breed="Pugs",
-        location="TEST",
-        description="TEST",
-        photo=read_photo("data/photos/0.jpg"),
+        name="Alex",
+        breed="German Shepherd Dogs",
+        location="Łódź",
+        description="Gwiazda polskiego serialu, zawsze gotowy do akcji. \
+            z takim psem na pewno polecą na Ciebie młode rude aspirantki. Szacunek na komendzie gwarantowany.",
+        photo=read_photo("data/photos/alex.jpg"),
         owner_id=0
     ))
+
+    dogs.append(Animal(
+        name="K-9000",
+        breed="Rottweilers",
+        location="Yara Island",
+        description="Wcielony diabeł, odgryzie jajca każdemu, kto zagraża jego panu. \
+            Legenda głosi, że Anton Castillo zachorował na gruźlicę jak go spotkał. \
+            Posiada dziwną dolegliwość, mianowicie jego sierść jest tytanowa, a oczy świecą mu się na czerwono. \
+            Podobno jego pierwszym właścicielem był T-1000",
+        photo=read_photo("data/photos/k9000.jpg"),
+        owner_id=0
+    ))
+
+    dogs.append(Animal(
+        name="Lampo",
+        breed="Anatolian Shepherd Dogs",
+        location="Piombino, Włochy",
+        description="Miłośnik kolei (MIKOL), wiernu swojemu Panu, bohaterski. Uratował życie małej dziewczynki ryzykując swoje własne.",
+        photo=read_photo("data/photos/pies koleją.jpg"),
+        owner_id=0
+    ))
+
+    dogs.append(Animal(
+        name="Lassie",
+        breed="Collies",
+        location="Szkocja",
+        description="Najwierniejsza suka jaką mamy w bazie. Nawet jak ją sprzedasz,\
+              wróci do Ciebie z powrotem choćby miała iść setki kilometrów przez góry.",
+        photo=read_photo("data/photos/lassie.jpg"),
+        owner_id=0
+    ))
+
+    dogs.append(Animal(
+        name="Scooby Doo",
+        breed="Pointers (German Shorthaired)",
+        location="Warner Bros Studio",
+        description="Trochę tchórzliwy, ale zawsze gotowy do pomocy. \
+            Uwielbia chrupki, ale nie przepada za duchami.",
+        photo=read_photo("data/photos/scooby.jpg"),
+        owner_id=0
+    ))
+
+    dogs.append(Animal(
+        name="Idefiks",
+        breed="Miniature Schnauzers",
+        location="Galia",
+        description="Niezwykle przyjazny piesek, uwielbia szynkę. Nie przepada za Rzymianami.",
+        photo=read_photo("data/photos/idefix.jpg"),
+        owner_id=0
+    ))
+
+    dogs.append(Animal(
+        name="Snoopy",
+        breed="Beagles",
+        location="Dach swojej budy",
+        description="Snoopy to piesek o wielkiej wyobraźni i jeszcze większym sercu.\
+            Uwielbia przesiadywać na dachu swojej budy i marzyć o byciu słynnym \
+            pilotem myśliwca albo pisarzem bestsellerów.",
+        photo=read_photo("data/photos/snoopy.png"),
+        owner_id=0
+    ))
+
+    dogs.append(Animal(
+        name="Clifford",
+        breed="Cavalier King Charles Spaniels",
+        location="Wielka Brytania",
+        description="Charakterystyczny czerwony kolor to nie jedyna cecha dzięki której nigdy go nie zgubisz\
+            Pomocny, choć często niezdarny. Pies koleżanki Twojej starej, Emily Elizabeth.",
+        photo=read_photo("data/photos/cliford.jpg"),
+        owner_id=0
+    ))
+
+    dogs.append(Animal(
+        name="Reksio",
+        breed="Fox Terriers (Wire)",
+        location="wieś pod Bielsko-Białą",
+        description="Uwielbia gonić ptaki, aktywnie włącza się w życie rodzinne i jest bardzo ambitny. \
+            Jego szczekanie rozpoznasz zawsze i wszędzie.",
+        photo=read_photo("data/photos/reksio.jpg"),
+        owner_id=0
+    ))
+
+    dogs.append(Animal(
+        name="Pankracy",
+        breed="Spaniels (Cocker)",
+        location="Warszawa, ul. Woronicza 17",
+        description="Niech wygląd Cię nie zmyli, to bardzo inteligentny pies.\
+            Teorie spiskowe mówią, że jego pojawienie i jednoczesne zniknięcie smoka Telesfora z anteny to nie przypadek.",
+        photo=read_photo("data/photos/pankracy.jpg"),
+        owner_id=0
+    ))
+
+    dogs.append(Animal(
+        name="Saba",
+        breed="Mastiffs",
+        location="Chartum oraz okoliczne pustynie i puszcze",
+        description="Radosny opiekuńczy pies, potrfi być bardzo waleczny. Przyjaźni się ze słoniami. \
+            Odważy się biec pod lufy porywaczy aby uratować swoich właścicieli. Stawi też czoła gorylom, ale będzie potrzebował małej pomocy słonia.",
+        photo=read_photo("data/photos/saba.jpg"),
+        owner_id=0
+    ))
+
+    dogs.append(Animal(
+        name="Tramp",
+        breed="Brittanys",
+        location="Midwest, USA",
+        description="Na początku będzie oziębły i nieufny, ale z czasem stanie się najlepszym przyjacielem. \
+            Lepiej trzymać go na smyczy bo lubi uciekać do swojej suczki.",
+        photo=read_photo("data/photos/zakochany.jpg"),
+        owner_id=0
+    ))
+
+    dogs.append(Animal(
+        name="Beethoven",
+        breed="St. Bernards",
+        location="USA",
+        description="Trochę niezdarny, bardzo silny i przyjazny. \
+            Obroni rodzinę jeśli nadarzy się taka potrzeba. ",
+        photo=read_photo("data/photos/bethoven.jpg"),
+        owner_id=0
+    ))
+    for i in range(1, 102):
+        dogs.append(Animal(
+            name="Dalmatyńczyk " + str(i),
+            breed="Dalmatians",
+            location="Londyn, Wielka Brytania",
+            description="Kolejny ze szczęśliwej rodzinki dalmatyńczyków. \
+                Lubi dzieci, ale nie przepada za starszymi kobietami w futrach",
+            photo=read_photo("data/photos/dalmatians.jpg"),
+            owner_id=0
+        ))
 
     for dog in dogs:
         if not row_exists(session, "name", dog.name, Animal):
