d26775ad68a26c9f4b0bba90fc908258a10672ee
Pawel Baran <pawel.baran2.stud@pw.edu.pl>
2024-06-06 23:54:28 +0200

    poprawki opisu i zmiana pozycji guzika do usuwania

--- src/website/pages/about.py
+++ src/website/pages/about.py
@@ -16,9 +16,6 @@ def main():
     # f.add_app_header()
     f.add_back_button()
 
-    with st.popover(":orange[Opcje deweloperskie]"):
-        st.button(label=":red[Czyszczenie tabeli \"Animals\"]", on_click=f.clear_animals_table_db)
-
     st.header("O aplikacji")
     st.markdown("""
     DogMatch jest aplikacją stworzoną aby rozwiązywać odwieczny lecz niedawno coraz bardziej zauważany problem. Osoby poszukujące psa do adopcji lub kupna,
@@ -46,9 +43,13 @@ def main():
     - Patryk Będkowski
     - Paweł Baran
 
+    Model został oparty o bibliotekę  [Recommenders Github](https://github.com/recommenders-team/recommenders). WYkorzystywany zbiór danych to [Dog Breeds](https://www.kaggle.com/datasets/sujaykapadnis/dog-breeds).
+                
     Aplikacja DogMatch powstała w ramach przedmiotu Zarządzanie Produktami Data science na Wydziale Elektroniki i Technik Informacyjnych Politechniki Warszawskiej. 
     """)
 
+    with st.popover(":orange[Opcje deweloperskie]"):
+        st.button(label=":red[Czyszczenie tabeli \"Animals\"]", on_click=f.clear_animals_table_db)
 
 if __name__ == "__main__":
     main()
